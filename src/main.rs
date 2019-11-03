c809c623b079dc60cc8693bb58ec3ba4f7f728e1
Martin Wallgren <martin@wallgren.it>
2019-11-03 14:58:53 +0100

    Add clap and initial arguments
    Method is optional and defaults to GET.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+#[macro_use]
+extern crate clap;
+
+use clap::{App, AppSettings, Arg};
+
+arg_enum! {
+    #[derive(Debug)]
+    pub enum Method {
+        POST,
+        GET,
+        PATCH,
+        DELETE,
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let opts = App::new("My Super Program")
+        .version(crate_version!())
+        .author(crate_authors!())
+        .setting(AppSettings::AllowMissingPositional)
+        .about("Http requests from the command line.")
+        .arg(
+            Arg::from_usage("<method> 'the http method to use'")
+                .possible_values(&Method::variants())
+                .required(true)
+                .default_value("GET"),
+        )
+        .arg(
+            Arg::with_name("url")
+                .help("the request url")
+                .required(true)
+                .takes_value(true),
+        )
+        .get_matches();
+
+    // unwrap safe due to url being mandatory
+    println!("{}", opts.value_of("url").unwrap());
 }
