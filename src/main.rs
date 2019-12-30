2a3d44b82349c2d9f3210d89f146517fef6f50de
Martin Wallgren <martin@wallgren.it>
2019-12-30 09:31:38 +0100

    Move argument handling to new cli module
    The code to configure clippy will probably grow a lot, so let's keep it
    in it's own module.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 #![warn(clippy::all)]
+pub mod cli;
 pub mod output;
 pub mod params;
 pub mod request;
@@ -8,52 +9,9 @@ use crate::request::*;
 
 #[macro_use]
 extern crate clap;
-use clap::{App, AppSettings, Arg, ArgMatches};
-
-arg_enum! {
-    #[derive(Debug)]
-    pub enum Method {
-        DELETE,
-        GET,
-        PATCH,
-        POST,
-        PUT,
-    }
-}
-
-fn get_args() -> ArgMatches<'static> {
-    App::new("rsurl")
-        .version(crate_version!())
-        .author(crate_authors!())
-        .setting(AppSettings::TrailingVarArg)
-        .about("Http requests from the command line.")
-        .arg(
-            Arg::with_name("body")
-                .short("b")
-                .long("body")
-                .value_name("FILE")
-                .takes_value(true)
-                .required(false),
-        )
-        .arg(
-            Arg::with_name("method")
-                .possible_values(&Method::variants())
-                .help("the http method to use")
-                .required(true),
-        )
-        .arg(Arg::with_name("url").help("the request url").required(true))
-        .arg(
-            Arg::with_name("item")
-                .help("extra request items such as headers, query params etc")
-                .multiple(true)
-                .validator(params::validate)
-                .required(false),
-        )
-        .get_matches()
-}
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let args = get_args();
+    let args = cli::get_args();
 
     // unwrap safe for mandatory values
     let url = args.value_of("url").unwrap();
